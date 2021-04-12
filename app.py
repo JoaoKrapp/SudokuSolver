2169b59b8422d535b53a2239c97cba5dcf041396
JoaoKrapp <joaokrappcurso@gmail.com>
2021-04-12 18:46:02 -0300

    Test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Created by Github.com/JoaoKrapp
+#Created by Github.com/JoaoKrapp testando vs code
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 from main import *
